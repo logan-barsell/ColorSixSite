112247fa2e44a28416ac1f7ef202e050051b6c6a
Logan Barsell <loganbarsell@logans-mbp.home>
2019-05-21 12:13:18 -0700

    package-json

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,7 @@
 $(document).ready( f => {
 
-	   $('.modal').modal()
+
+	$('.modal').modal()
 
 	var windowTop = $(window).scrollTop()
 	var coverPage = $('#coverPage').offset().top
@@ -26,45 +27,17 @@ $(document).ready( f => {
 		var aboutusPage = $('#aboutus').offset().top
 
 
-		if (windowTop > coverPage) {
+		if (windowTop > coverPage && $('#coverPage').is(':visible')) {
 			$('#coverPage').slideUp('slow', 'swing')
 			topNav.slideUp('slow', 'swing')
 			bottomNav.slideDown('slow', 'swing')
 		}
 
-		if (windowTop == navPage - 1) {
+		if (windowTop == navPage - 1 && $('#navPage').is(':visible')) {
 			$('#coverPage').slideDown('slow', 'swing')
 			topNav.slideDown('slow', 'swing')
 			bottomNav.slideUp('slow', 'swing')
 		}
-
-		// if (windowTop == eventsPage - 1) {
-		// 	$('#coverPage').slideDown('slow', 'swing')
-		// 	topNav.slideDown('slow', 'swing')
-		// 	bottomNav.slideUp('slow', 'swing')
-		// 	mainNav.css({'margin-top':'-1px','top':'auto'})
-		// }
-
-		// if (windowTop == musicPage - 1) {
-		// 	$('#coverPage').slideDown('slow', 'swing')
-		// 	topNav.slideDown('slow', 'swing')
-		// 	bottomNav.slideUp('slow', 'swing')
-		// 	mainNav.css({'margin-top':'-1px','top':'auto'})
-		// }
-
-		// if (windowTop == videosPage - 1) {
-		// 	$('#coverPage').slideDown('slow', 'swing')
-		// 	topNav.slideDown('slow', 'swing')
-		// 	bottomNav.slideUp('slow', 'swing')
-		// 	mainNav.css({'margin-top':'-1px','top':'auto'})
-		// }
-
-		// if (windowTop == aboutusPage - 1) {
-		// 	$('#coverPage').slideDown('slow', 'swing')
-		// 	topNav.slideDown('slow', 'swing')
-		// 	bottomNav.slideUp('slow', 'swing')
-		// 	mainNav.css({'margin-top':'-1px','top':'auto'})
-		// }
 
 	})
 
@@ -77,9 +50,7 @@ $(document).ready( f => {
 	$('#navPage li.collection-item').click( f => {
 		$('#navPage').animate({width: 'toggle'},500)
 		$('#mainNav').animate({width: 'toggle'}, 500)
-		$('#navBottom').animate({width: 'toggle'}, 500, f => {
-			$('#navBottom').css('opacity','0')
-		})
+		$('#navBottom').animate({width: 'toggle'}, 500)
 	})
 
 	$('#link2events').click( f => {
@@ -87,14 +58,14 @@ $(document).ready( f => {
 	})
 
 	$('#link2music').click( f => {
-		$('#music').slideDown(1500)
+		$('#music').slideDown(2500)
 	})
 
 	$('#link2videos').click( f => {
 		$('#videos').slideDown(1500)
 	})
 	$('#link2aboutus').click( f => {
-		$('#aboutus').slideDown(2700)
+		$('#aboutus').slideDown(3500)
 	})
 
 	$('#back2menu').click( f => {
@@ -102,7 +73,6 @@ $(document).ready( f => {
 		$('.page').hide()
 		$('#navPage').animate({width: 'toggle'}, 500)
 		$('#mainNav').animate({width: 'toggle'}, 500)
-		$('#navBottom').css('opacity','.9')
 		$('#navBottom').animate({width: 'toggle'}, 500)
 		$('html, body').animate({scrollTop:$(document).height()}, 'fast')
 	})
@@ -158,28 +128,13 @@ $(document).ready( f => {
         // initialize playlist and controls
         var index = 0,
             playing = false,
-            mediaPath = 'https://archive.org/download/mythium/',
+            mediaPath = '../static/css/audio/',
             extension = '',
             tracks = [{
                 "track": 1,
                 "name": "Dirty Little Secret",
                 "duration": "3:14",
-                "file": "JLS_ATI"
-            }, {
-                "track": 2,
-                "name": "Goodbye, Goodbye",
-                "duration": "3:18",
-                "file": "BS_TF"
-            }, {
-                "track": 3,
-                "name": "Sweet Hole In My Head",
-                "duration": "4:32",
-                "file": "BS_ATKM"
-            }, {
-                "track": 4,
-                "name": "The Bends",
-                "duration": "2:51",
-                "file": "BSFM_TF"
+                "file": "dls"
             }],
             buildPlaylist = $(tracks).each(function(key, value) {
                 var trackNumber = value.track,
@@ -199,13 +154,11 @@ $(document).ready( f => {
             npAction = $('#npAction'),
             npTitle = $('#npTitle'),
             audio = $('#audio1').on('play', function () {
+            	audio2.pause()
                 playing = true
-                npAction.text('Now Playing...')
             }).on('pause', function () {
                 playing = false
-                npAction.text('Paused...')
             }).on('ended', function () {
-                npAction.text('Paused...')
                 if ((index + 1) < trackCount) {
                     index++
                     loadTrack(index)
@@ -265,6 +218,130 @@ $(document).ready( f => {
         // boo hoo
         $('.column').addClass('hidden')
         var noSupport = $('#audio1').text()
+        $('.container').append('<p class="no-support">' + noSupport + '</p>')
+    }
+
+
+
+    var supportsAudio2 = !!document.createElement('audio2').canPlayType
+    if (supportsAudio) {
+    var player2 = new Plyr('#audio3', {
+            controls: [
+                'restart',
+                'play',
+                'progress',
+                'current-time',
+                'duration',
+                'mute',
+                'volume'
+            ]
+        })
+        // initialize playlist and controls
+        var index = 0,
+            playing = false,
+            mediaPath = '../static/css/audio/',
+            extension = '',
+            tracks2 = [{
+                "track": 1,
+                "name": "Goodbye, Goodbye",
+                "duration": "3:18",
+                "file": "gg"
+            }, {
+                "track": 2,
+                "name": "Sweet Hole In My Head",
+                "duration": "4:32",
+                "file": "shimh"
+            }, {
+                "track": 3,
+                "name": "The Bends",
+                "duration": "2:51",
+                "file": "bends"
+            }],
+            buildPlaylist2 = $(tracks2).each(function(key, value) {
+                var trackNumber2 = value.track,
+                    trackName2 = value.name,
+                    trackDuration2 = value.duration
+                if (trackNumber2.toString().length === 1) {
+                    trackNumber2 = '0' + trackNumber2
+                }
+                $('#plList2').append('<li> \
+                    <div class="plItem"> \
+                        <span class="plTitle">' + trackName2 + '</span> \
+                        <span class="plLength">' + trackDuration2 + '</span> \
+                    </div> \
+                </li>')
+            }),
+            trackCount2 = tracks2.length,
+            npAction2 = $('#npAction2'),
+            npTitle2 = $('#npTitle2'),
+            audio2 = $('#audio3').on('play', function () {
+            	audio.pause()
+                playing = true
+                npAction2.text('Now Playing...')
+            }).on('pause', function () {
+                playing = false
+                npAction2.text('Paused...')
+            }).on('ended', function () {
+                npAction2.text('Paused...')
+                if ((index + 1) < trackCount2) {
+                    index++
+                    loadTrack(index)
+                    audio2.play()
+                } else {
+                    audio2.pause()
+                    index = 0
+                    loadTrack(index)
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').on('click', function () {
+                if ((index - 1) > -1) {
+                    index--
+                    loadTrack(index)
+                    if (playing) {
+                        audio2.play()
+                    }
+                } else {
+                    audio2.pause()
+                    index = 0
+                    loadTrack(index)
+                }
+            }),
+            btnNext = $('#btnNext').on('click', function () {
+                if ((index + 1) < trackCount2) {
+                    index++
+                    loadTrack(index)
+                    if (playing) {
+                        audio2.play()
+                    }
+                } else {
+                    audio2.pause()
+                    index = 0
+                    loadTrack(index)
+                }
+            }),
+            li = $('#plList2 li').on('click', function () {
+                var id = parseInt($(this).index())
+                if (id !== index) {
+                    playTrack(id)
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel2').removeClass('plSel2')
+                $('#plList2 li:eq(' + id + ')').addClass('plSel2')
+                npTitle2.text(tracks2[id].name)
+                index = id
+                audio2.src = mediaPath + tracks2[id].file + extension
+            },
+            playTrack = function (id) {
+                loadTrack(id)
+                audio2.play()
+            }
+        extension = audio2.canPlayType('audio/mpeg') ? '.mp3' : audio2.canPlayType('audio/ogg') ? '.ogg' : ''
+        loadTrack(index)
+    } else {
+        // boo hoo
+        $('.column').addClass('hidden')
+        var noSupport = $('#audio3').text()
         $('.container').append('<p class="no-support">' + noSupport + '</p>')
     }
 
